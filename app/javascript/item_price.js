349d98ba2aaa66b965eac4eba8522d4214d41900
good39original <aoiumi442001@yahoo.co.jp>
2021-09-14 23:09:53 +0900

    fix#2

--- app/javascript/item_price.js
+++
@@ -1,9 +0,0 @@
-const priceInput = document.getElementById("金額を入力する場所のid");
-priceInput.addEventListener("input", () => {
-   console.log("イベント発火");
-})
-  
-
-//window.addEventListener('load', () => {
-  //console.log("OK");
-//});
