8c16be7df6727f888ec693eaecf4489b6cccd6dc
good39original <aoiumi442001@yahoo.co.jp>
2021-09-24 20:34:08 +0900

    商品購入機能#2

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,10 +3,11 @@ class  Order
   attr_accessor :postal_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number
 
   with_options presence: true do
-    validates :postal_code,       format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid."}
+    validates :postal_code,       format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :municipalities,    format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :address           
-    validates :telephone_number,  format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
+    validates :telephone_number,  format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is too short."}
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 end
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '新規購入できるとき' do
+    it 'すべての入力値が正しく入力されていれば登録できる' do
+      expect(@user).to be_valid
+    end
+    context '購入できないとき' do
+      it "住所が空だと購入できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it "郵便番号が空だと購入できない" do
+      end
+      it "郵便番号が空だと購入できない" do
+      end
+
 end
